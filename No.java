76d5778da0ab1dd15ddb0f52341d006c5a44e3d8
camilyolivei <camilyguilhermeoliveira@gmail.com>
2025-12-04 01:16:12 -0300

    Criando class No e No Binario

---
+++ No.java
@@ -0,0 +1,11 @@
+class No {
+    int valor;
+    int altura;
+    No esquerda;
+    No direita;
+
+    No(int valor) {
+        this.valor = valor;
+        this.altura = 1;
+    }
+}
---
+++ NoB.java
@@ -0,0 +1,11 @@
+class NoB {
+    int valor;
+    NoB esquerda;
+    NoB direita;
+
+    public NoB(int valor) {
+        this.valor = valor;
+        this.esquerda = null;
+        this.direita = null;
+    }
+}
