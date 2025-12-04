5c7627fd8e0e1cf75bfdf2a15e106abb4bf235bc
camilyolivei <camilyguilhermeoliveira@gmail.com>
2025-12-04 20:06:46 -0300

    Criando class RelatorioAVL

---
+++ RelatorioAVL.java
@@ -0,0 +1,16 @@
+public class RelatorioAVL {
+
+  public void relatorioInsercaoOrdenado(int quantidadeNumero){
+    AVL arvore = new AVL();
+    for(int i = 0; i < quantidadeNumero; i++){
+      arvore.adicionar(i);
+    }
+  }
+
+  public void relatorioInsercaoAleatorio(int quantidadeNumero){
+    AVL arvore = new AVL();
+
+    for(int i = 0; i < quantidadeNumero; i++){
+      arvore.adicionar((int)(Math.random() * quantidadeNumero + 1));
+    }
+  }
